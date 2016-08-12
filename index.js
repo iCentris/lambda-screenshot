d9e882e47ee38363e2cda34eb5d757e83b829866
keithbranton <github@branton.co.uk>
2016-08-12 17:40:49 -0600

    PYR-17624 customize forked project for eCards
    Use key for temp file name, so that the file type is controller by vibe
    Detect the size of the .ecard-body div and set the clip region to match
    Use page.onLoadFinished as it is way simpler

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var AWS = require('aws-sdk');
 var fs = require('fs');
 
 exports.handler = function (event, context, callback) {
-    var args = event.Records[0].Sns.MessageAttributes;
+    var args = event.Records[0].Sns.MessageAttributes,
+        key = args.Key.Value;
 
     // Set the path as described here: https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
     process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
@@ -12,13 +13,15 @@ exports.handler = function (event, context, callback) {
     // Set the path to the phantomjs binary
     var phantomPath = path.join(__dirname, 'phantomjs_linux-x86_64');
 
-    var target = '/tmp/' + Math.floor(Math.random() * 1000000) + '.png';
+    var target = '/tmp/' + key;
+
     // Arguments for the phantom script
     var processArgs = ['--ssl-protocol=any',
         '--ignore-ssl-errors=true',
         '--disk-cache=true',
         path.join(__dirname, 'phantom-script.js'),
-        args.Url.Value, target];
+        args.Url.Value,
+        target];
 
     // Launch the child process
     childProcess.execFile(phantomPath, processArgs, {}, function (error, stdout, stderr) {
@@ -34,7 +37,7 @@ exports.handler = function (event, context, callback) {
         //upload the file to s3
         new AWS.S3().upload({
             Bucket: args.Bucket.Value,
-            Key: args.Key.Value,
+            Key: key,
             ContentType: 'image/png',
             Body: fs.createReadStream(target)
         }, function (err, data) {
--- phantom-script.js
+++ phantom-script.js
@@ -1,34 +1,21 @@
 var system = require('system');
 var page = require('webpage').create();
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
-page.viewportSize = { width: 1000, height: 1000 }
-page.clipRect = { top: 0, left: 0, width: 1000, height: 1000 }
+page.viewportSize = { width: 2000, height: 2000 }
 
 // Passed in from parent process
 var url = system.args[1];
 var target = system.args[2];
 
-page.open(url, function start(status) {
+page.onLoadFinished = function() {
+  page.clipRect = page.evaluateJavaScript( "function() { var element = jQuery('.ecard-body'); return jQuery.extend(element.offset(), {width: element.outerWidth(), height: element.outerHeight()});}" );
+  page.render(target);
+  phantom.exit();
+}
 
+page.open(url, function start(status) {
   // Set the background to white, just in case
   page.evaluate(function() {
     document.body.style.background = 'white';
   });
-
-  function checkReadyState() {
-    setTimeout(function () {
-      var readyState = page.evaluate(function () {
-        return document.readyState;
-      });
-
-      if ("complete" === readyState) {
-        page.render(target);
-        phantom.exit();
-      } else {
-        checkReadyState();
-      }
-    });
-  }
-
-  checkReadyState();
 });
