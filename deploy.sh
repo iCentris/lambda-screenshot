bb51f9b76b934a2664d6f6f9b11e4377035ccfba
keithbranton <github@branton.co.uk>
2016-08-12 09:21:37 -0600

    PYR-17624 customize forked project for eCards

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+file=url2image.zip
+
+if [ -f $file ]; then
+  echo "Refreshing the archive..."
+  zip -f $file
+else
+  echo "Creating the archive..."
+  zip $file index.js phantom-script.js phantomjs_linux-x86_64
+fi
+
+echo "Archive is ready"
+echo "Publishing archive to aws..."
+aws lambda update-function-code --function-name url2image-development --zip-file fileb://$file --publish
+echo "Finished."
