9639d22750247e55e5de19b0f6f7f35ebfbe2f0f
keithbranton <github@branton.co.uk>
2016-08-15 09:01:09 -0600

    PYR-17624 customize forked project for eCards
    Reformat code

--- index.js
+++ index.js
@@ -4,48 +4,50 @@ var AWS = require('aws-sdk');
 var fs = require('fs');
 
 exports.handler = function (event, context, callback) {
-    var args = event.Records[0].Sns.MessageAttributes,
-        key = args.Key.Value;
+  var args = event.Records[0].Sns.MessageAttributes,
+      key = args.Key.Value;
 
-    // Set the path as described here: https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
-    process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
+  // Set the path as described here: https://aws.amazon.com/blogs/compute/running-executables-in-aws-lambda/
+  process.env['PATH'] += ':' + process.env['LAMBDA_TASK_ROOT'];
 
-    // Set the path to the phantomjs binary
-    var phantomPath = path.join(__dirname, 'phantomjs_linux-x86_64');
+  // Set the path to the phantomjs binary
+  var phantomPath = path.join(__dirname, 'phantomjs_linux-x86_64');
 
-    var target = '/tmp/' + key;
+  var target = '/tmp/' + key;
 
-    // Arguments for the phantom script
-    var processArgs = ['--ssl-protocol=any',
-        '--ignore-ssl-errors=true',
-        '--disk-cache=true',
-        path.join(__dirname, 'phantom-script.js'),
-        args.Url.Value,
-        target];
+  // Arguments for the phantom script
+  var processArgs = [
+    '--ssl-protocol=any',
+    '--ignore-ssl-errors=true',
+    '--disk-cache=true',
+    path.join(__dirname, 'phantom-script.js'),
+    args.Url.Value,
+    target
+  ];
 
-    // Launch the child process
-    childProcess.execFile(phantomPath, processArgs, {}, function (error, stdout, stderr) {
-        if (error) {
-            callback(error);
-            return;
-        }
-        if (stderr) {
-            callback(error);
-            return;
-        }
+  // Launch the child process
+  childProcess.execFile(phantomPath, processArgs, {}, function (error, stdout, stderr) {
+    if (error) {
+      callback(error);
+      return;
+    }
+    if (stderr) {
+      callback(error);
+      return;
+    }
 
-        //upload the file to s3
-        new AWS.S3().upload({
-            Bucket: args.Bucket.Value,
-            Key: key,
-            ContentType: 'image/png',
-            Body: fs.createReadStream(target)
-        }, function (err, data) {
-            if (err) {
-                callback(err);
-            } else {
-                callback(null, 'Done!');
-            }
-        });
+    //upload the file to s3
+    new AWS.S3().upload({
+      Bucket: args.Bucket.Value,
+      Key: key,
+      ContentType: 'image/png',
+      Body: fs.createReadStream(target)
+    }, function (err, data) {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, 'Done!');
+      }
     });
+  });
 }
--- phantom-script.js
+++ phantom-script.js
@@ -1,21 +1,22 @@
 var system = require('system');
 var page = require('webpage').create();
 page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
-page.viewportSize = { width: 2000, height: 2000 }
+page.viewportSize = {width: 2000, height: 2000}
 
 // Passed in from parent process
-var url = system.args[1];
-var target = system.args[2];
+var url = system.args[1]; //url to copy the image from
+var target = system.args[2]; //local file name to write the image to
 
-page.onLoadFinished = function() {
-  page.clipRect = page.evaluateJavaScript( "function() { var element = jQuery('.ecard-body'); return jQuery.extend(element.offset(), {width: element.outerWidth(), height: element.outerHeight()});}" );
+page.onLoadFinished = function () {
+  // set the clip region to match the .ecard-body size
+  page.clipRect = page.evaluateJavaScript("function() { var element = jQuery('.ecard-body'); return jQuery.extend(element.offset(), {width: element.outerWidth(), height: element.outerHeight()});}");
   page.render(target);
   phantom.exit();
 }
 
 page.open(url, function start(status) {
   // Set the background to white, just in case
-  page.evaluate(function() {
+  page.evaluate(function () {
     document.body.style.background = 'white';
   });
 });
